75b3e56f9ca43d670605974b7621d3d3adc1ca6a
Srinivas Rao Tammireddy <88500103+srinu2003@users.noreply.github.com>
2023-08-29 19:35:44 +0530

    Update HammingCode.c

--- HammingCode.c
+++ HammingCode.c
@@ -1,45 +1,41 @@
 #include<stdio.h> 
 void main(){
+    //input:  1 0 1 0 0 1 0 1 0 0 1
+    //output: 0 0 1 1 1 0 1 0 0 0 1 0 1 0 0 1
     int n;
-    printf("Enter no.of bits: ");
+    printf("Enter size of msg block: ");
     scanf("%d",&n);
-    printf("Enter %d bits:",n);
+
+    printf("Enter data bits: ");
     int msg[n];
 
     int parity = 0,p[4];
-    for (int i = 0; i < n; i++)
-    {
-        if (i && (!(i & (i - 1))))
-        {
-            msg[i] = 0;
-            continue;
+    for (int i = 0; i < n; i++) {
+        if ((i & (i - 1))) {
+            scanf("%d",&msg[i]);
+            if (msg[i]==1) { parity ^=(i); }
         }
-        
+    }
+    printf("Message block: ");
+    for (int i = 0; i < n; i++) {
+        if (!(i & (i - 1))){
+            msg[i]=parity & 1;
+            parity >>= 1;
+        }
+        printf("%d ",msg[i]);
+    }
+    printf("\nEnter senders msg block:");
+    printf("%d",parity);
+    for (int i = 1; i < n; i++) {
         scanf("%d",&msg[i]);
         if (msg[i]==1) { parity ^=(i); }
-        // printf("%d\n",parity);
-        // p[parity%4] = 0;
     }
-    int i =1;
-    while (parity>0)
-    {
-        msg[i] = parity%2;
-        parity /=2;
-        i *=2;
-        printf("%d ",i);
-    }
-    printf("Message block:");
-    for (int j = 0; j < i; j++) 
-    {
-        if (msg[j]==1) { parity ^=(j); } else{msg[i] = 0;}
-        printf(" %d ",msg[j]);
-        // printf("%d\n",parity);
-        // p[parity%4] = 0;
+    for (int i = 1; i < n; i++) {
+        printf("%d ",msg[i]);
     }
     if (parity==0) {
         printf("\nReciver said \"No Unibit error :)\"");
     }else {
         printf("\nReciver said \"Error at position: %d!\"",parity);
     }
-
 }

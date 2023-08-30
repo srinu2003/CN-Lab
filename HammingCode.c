2fb9eefe1132f6050e10990dc0122a45ab93ab76
srinu2003 <tsrin2003@gmail.com>
2023-08-30 22:23:02 +0530

    Its wrong!! 😢

--- HammingCode.c
+++ HammingCode.c
@@ -1,15 +1,19 @@
-#include<stdio.h> 
-void main(){
-    //input:  1 0 1 0 0 1 0 1 0 0 1
-    //output: 0 0 1 1 1 0 1 0 0 0 1 0 1 0 0 1
+#include<stdio.h>
+#include<stdlib.h>
+void main() {
+    //input:(15,11)  1 0 1 0 0 1 0 1 0 0 1
+    //output:(15,11) 0 0 1 1 1 0 1 0 0 0 1 0 1 0 0 1
+    //input:(7,4)  1 0 1 0
+    //output:(7,4) 1 0 1 1 0 0 1 0
     int n;
     printf("Enter size of msg block: ");
     scanf("%d",&n);
+    n++;
+    // printf("%d",n);
+    printf("Enter data bits: ");    
+    int *msg = (int*) malloc(n * sizeof(int));
 
-    printf("Enter data bits: ");
-    int msg[n];
-
-    int parity = 0,p[4];
+    int parity = 0,p;
     for (int i = 0; i < n; i++) {
         if ((i & (i - 1))) {
             scanf("%d",&msg[i]);
@@ -22,11 +26,12 @@ void main(){
             msg[i]=parity & 1;
             parity >>= 1;
         }
-        printf("%d ",msg[i]);
+        // printf("%d:%d, ",i,msg[i]);
+        printf("%d, ",msg[i]);
     }
     printf("\nEnter senders msg block:");
-    printf("%d",parity);
-    for (int i = 1; i < n; i++) {
+    // printf("%d",parity);
+    for (int i = 0; i < n; i++) {
         scanf("%d",&msg[i]);
         if (msg[i]==1) { parity ^=(i); }
     }

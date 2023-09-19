978c4712d8590bae9b04c74f356d7ca4a4ae01f8
Srinivas Rao Tammireddy <88500103+srinu2003@users.noreply.github.com>
2023-09-19 15:39:57 +0530

    Update CyclicRC.c

--- CyclicRC.c
+++ CyclicRC.c
@@ -11,7 +11,7 @@ int main() {
     int m = strlen(message);
     int n = strlen(divisor);
 
-    // Append zeros for CRC
+
     for (int i = 0; i < n - 1; i++) {
         message[m + i] = '0';
     }

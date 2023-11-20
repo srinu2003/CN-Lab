752c257020f4d58d936a8323700b6062536b11d7
srinu2003 <tsrin2003@gmail.com>
2023-11-20 21:33:12 +0530

    DVR made readable

--- Lab-8/DVR.c
+++ Lab-8/DVR.c
@@ -1,20 +1,21 @@
 #include<stdio.h>
 struct node {
-        unsigned dist[20];
+        unsigned cost[20];
         unsigned from[20];
 } rt[10];
 int main() {
-        int dmat[20][20];
+        int costMat[20][20];
         int n,i,j,k,count=0;
         printf("\nEnter the number of nodes: ");
-        scanf("%d",&n);printf("Enter the cost matrix:\n");
+        scanf("%d",&n);
+        printf("Enter the cost matrix:\n");
 
         for(i=0;i<n;i++){
             for(j=0;j<n;j++) {
-                scanf("%d",&dmat[i][j]);
+                scanf("%d",&costMat[i][j]);
 
-                dmat[i][i]    = 0;
-                rt[i].dist[j] = dmat[i][j];
+                costMat[i][i] = 0;
+                rt[i].cost[j] = costMat[i][j];
                 rt[i].from[j] = j;
             }
         }
@@ -23,8 +24,8 @@ int main() {
             for(i = 0; i < n; i++){
                 for(j = 0; j < n; j++){
                     for(k = 0; k < n; k++){
-                        if(rt[i].dist[j] > dmat[i][k]+rt[k].dist[j]) {
-                            rt[i].dist[j] = rt[i].dist[k]+rt[k].dist[j];
+                        if( rt[i].cost[j] > costMat[i][k] + rt[k].cost[j]) {
+                            rt[i].cost[j] = rt[i].cost[k] + rt[k].cost[j];
                             rt[i].from[j] = k;
                             count++;
                         }
@@ -36,7 +37,7 @@ int main() {
         for(i=0;i<n;i++) {
             printf("\nState value for router %d is:\n",i+1);
             for(j=0;j<n;j++) {
-                printf("Node %d via %d Distance: %d\n",j+1,rt[i].from[j]+1,rt[i].dist[j]);
+                printf("Node %d via %d Distance: %d\n",j+1,rt[i].from[j]+1,rt[i].cost[j]);
             }
         }
         printf("\n");

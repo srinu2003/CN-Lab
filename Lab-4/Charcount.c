4200a76afa914761b9bce9588d1797334c4f660a
srinu2003 <tsrin2003@gmail.com>
2023-12-16 15:52:14 +0530

    CN Complete

---
+++ Lab-4/Charcount.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+char data[20][20];
+int n;
+void main()
+{
+    int i, ch, j;
+    char tmp[20][20];
+    printf("Enter the number of frames:");
+    scanf("%d", &n);
+    for (i = 0; i <= n; i++)
+    {
+        if (i != 0)
+        {
+            printf("Frame%d: ", i);
+            scanf("%s", &data[i]);
+        }
+    }
+    for (i = 0; i <= n; i++)
+    {
+        tmp[i][0] = 49 + strlen(data[i]);
+        tmp[i][1] = '\0';
+        strcat(tmp[i], data[i]);
+    }
+    printf("\n\t\t AT THE SENDER:\n");
+    printf("Data as frames:\n");
+    for (i = 1; i <= n; i++)
+    {
+        printf("frame%d: ", i);
+        puts(tmp[i]);
+    }
+    printf("Data transmitted: ");
+    for (i = 1; i <= n; i++)
+        printf("%s", tmp[i]);
+    printf("\n\t\t AT THE RECEIVER\n");
+    printf("Data received after removing count char: ");
+    for (i = 1; i <= n; i++)
+    {
+        ch = (int)(tmp[i][0] - 49);
+        for (j = 1; j <= ch; j++)
+            data[i][j - 1] = tmp[i][j];
+        data[i][j - 1] = '\0';
+    }
+    for (i = 1; i <= n; i++)
+        printf("%s", data[i]);
+    printf("\nData as frames:\n");
+    for (i = 1; i <= n; i++)
+    {
+        printf("Frame%d: ", i);
+        puts(data[i]);
+    }
+}
--- Lab-4/Charstuffing.c
+++ Lab-4/Charstuffing.c
@@ -1,1 +1,80 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void charc();
+void main()
+{
+    int choice;
+    while (1)
+    {
+        printf("\n\n 1.Character stuffing");
+        printf("\n 2.Exit");
+        printf("\n Enter choice: ");
+        scanf("%d", &choice);
+        if (choice > 2)
+            printf("\n\n invalid option...please re-enter");
+        switch (choice)
+        {
+        case 1:
+            charc();
+            break;
+        case 2:
+            exit(0);
+        }
+    }
+}
+void charc(void)
+{
+    char c[50], d[50], t[50];
+    int i, m, j;
+    printf("Enter the number of characters:\n");
+    scanf("%d", &m);
+    printf("Enter the characters:\n");
+    for (i = 0; i < m + 1; i++)
+    {
+        scanf("%c", &c[i]);
+    }
+    printf("\nOriginal data: ");
+    for (i = 0; i < m + 1; i++)
+        printf("%c", c[i]);
+    d[0] = 'd';
+    d[1] = 'l';
+    d[2] = 'e';
+    d[3] = 's';
+    d[4] = 't';
+    d[5] = 'x';
+    for (i = 0, j = 6; i < m + 1; i++, j++)
+    {
+        if ((c[i] == 'd' && c[i + 1] == 'l' && c[i + 2] == 'e'))
+        {
+            d[j++] = 'd';
+            d[j++] = 'l';
+            d[j++] = 'e';
+            m = m + 3;
+        }
+        d[j] = c[i];
+    }
+    m = m + 6; m++;
+    d[m++] = 'd';
+    d[m++] = 'l';
+    d[m++] = 'e';
+    d[m++] = 's';
+    d[m++] = 't';
+    d[m++] = 'x';
+    printf("\n\nTransmitted data:\n");
+    for (i = 0; i < m; i++)
+    {
+        printf("%c", d[i]);
+    }
+    for (i = 6, j = 0; i < m - 6; i++, j++)
+    {
+        if (d[i] == 'd' && d[i + 1] == 'l' && d[i + 2] == 'e' && d[i + 3] == 'd' && d[i + 4] == 'l' && d[i + 5] == 'e')
+            i = i + 3;
+        t[j] = d[i];
+    }
+    printf("\n\nReceived data: ");
+    for (i = 0; i < j; i++)
+    {
+        printf("%c", t[i]);
+    }
+}

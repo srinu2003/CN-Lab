e1a1d765b6924c7f4ef94eea152e715df0e8ba4d
srinu2003 <tsrin2003@gmail.com>
2023-12-22 01:59:30 +0530

    Fix char stuffing bug and implement sliding window protocol

--- Lab-4/Charstuffing.c
+++ Lab-4/Charstuffing.c
@@ -30,31 +30,24 @@ void charc(void)
     printf("Enter the number of characters:\n");
     scanf("%d", &m);
     printf("Enter the characters:\n");
-    for (i = 0; i < m + 1; i++)
-    {
+    for (i = 0; i < m + 1; i++) {
         scanf("%c", &c[i]);
     }
     printf("\nOriginal data: ");
     for (i = 0; i < m + 1; i++)
         printf("%c", c[i]);
-    d[0] = 'd';
-    d[1] = 'l';
-    d[2] = 'e';
-    d[3] = 's';
-    d[4] = 't';
-    d[5] = 'x';
-    for (i = 0, j = 6; i < m + 1; i++, j++)
-    {
+    strcpy(d, "dlexstx");
+    for (i = 0, j = 6; i < m + 1; i++, j++) {
         if ((c[i] == 'd' && c[i + 1] == 'l' && c[i + 2] == 'e'))
         {
             d[j++] = 'd';
             d[j++] = 'l';
             d[j++] = 'e';
-            m = m + 3;
+            m += 3;
         }
         d[j] = c[i];
     }
-    m = m + 6; m++;
+    m = m + 7;
     d[m++] = 'd';
     d[m++] = 'l';
     d[m++] = 'e';
@@ -62,12 +55,10 @@ void charc(void)
     d[m++] = 't';
     d[m++] = 'x';
     printf("\n\nTransmitted data:\n");
-    for (i = 0; i < m; i++)
-    {
+    for (i = 0; i < m; i++) {
         printf("%c", d[i]);
     }
-    for (i = 6, j = 0; i < m - 6; i++, j++)
-    {
+    for (i = 6, j = 0; i < m - 6; i++, j++) {
         if (d[i] == 'd' && d[i + 1] == 'l' && d[i + 2] == 'e' && d[i + 3] == 'd' && d[i + 4] == 'l' && d[i + 5] == 'e')
             i = i + 3;
         t[j] = d[i];
